41f37cc49b3ea8a83964a130f0e0218f620b7173
2359media <2359media@2359medias-MacBook-Air.local>
2016-12-13 17:03:12 +0700

    Refactor

--- scripts/deploy_hooks/after_install.sh
+++ scripts/deploy_hooks/after_install.sh
@@ -3,10 +3,12 @@
 TMP_DIR="/tmp"
 CODE_DIR="/home/ec2-user/condo_admin"
 
+echo $whoami
+
 if [ -f $TMP_DIR/.env ]; then
   cp $TMP_DIR/.env $CODE_DIR/.env
 else
-  cp $CODE_DIR/.env.example $CODE_DIR/.env  
+  cp $CODE_DIR/.env.example $CODE_DIR/.env
 fi
 
 npm install
