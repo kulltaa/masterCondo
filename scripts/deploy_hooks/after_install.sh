6a04b401a0d1be5fe2e56beeb0132620484d811f
2359media <2359media@2359medias-MacBook-Air.local>
2016-12-13 16:50:55 +0700

    Refactor

--- scripts/deploy_hooks/after_install.sh
+++ scripts/deploy_hooks/after_install.sh
@@ -3,5 +3,9 @@
 TMP_DIR="/tmp"
 CODE_DIR="/home/ec2-user/condo_admin"
 
+if [ ! -d $CODE_DIR ]; then
+  exit 0
+fi
+
 cp $TMP_DIR/.env $CODE_DIR/.env
 npm install
--- scripts/deploy_hooks/before_install.sh
+++ scripts/deploy_hooks/before_install.sh
@@ -3,6 +3,10 @@
 TMP_DIR="/tmp"
 CODE_DIR="/home/ec2-user/condo_admin"
 
+if [ ! -d $CODE_DIR ]; then
+  exit 0
+fi
+
 if [ ! -f $CODE_DIR/.env ]; then
   cp $CODE_DIR/.env.example $CODE_DIR/.env
 fi
